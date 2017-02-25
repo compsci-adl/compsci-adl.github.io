bf26b974cbd8973ad8f4988c94556e71ea39a989
Jake Lane <me@jakelane.me>
2017-02-25 15:31:45 +1030

    Reformat JavaScript files

--- js/csclub.common.js
+++ js/csclub.common.js
@@ -1,11 +1,11 @@
 // Redirect to https
 var host = "csclub.org.au";
 if ((host == window.location.host) && (window.location.protocol != "https:")) {
-  window.location.protocol = "https:";
+    window.location.protocol = "https:";
 }
-$( document ).ready(function() {
-  // Initiate mobile nav
-  $(".button-collapse").sideNav();
+$(document).ready(function() {
+    // Initiate mobile nav
+    $(".button-collapse").sideNav();
 });
 
 
@@ -14,19 +14,19 @@ $( document ).ready(function() {
  * @return {boolean} True if the element is within the viewport.
  */
 function isElementInViewport(el) {
-  // Special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
 
-  var rect = el.getBoundingClientRect();
+    var rect = el.getBoundingClientRect();
 
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
-  );
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
 }
 
 /*
@@ -34,27 +34,27 @@ function isElementInViewport(el) {
  * @return {Object} An object containing data from form inputs.
  */
 function formToJson(form) {
-  var array = $(form).serializeArray();
-  var json = {};
+    var array = $(form).serializeArray();
+    var json = {};
 
-  $.each(array, function () {
-    if (this.value !== 'other') {
-      if (json[this.name] !== undefined) {
-        if (!json[this.name].push) {
-          json[this.name] = [json[this.name]];
+    $.each(array, function() {
+        if (this.value !== 'other') {
+            if (json[this.name] !== undefined) {
+                if (!json[this.name].push) {
+                    json[this.name] = [json[this.name]];
+                }
+                json[this.name].push(this.value || '');
+            } else {
+                json[this.name] = this.value || '';
+            }
         }
-        json[this.name].push(this.value || '');
-      } else {
-        json[this.name] = this.value || '';
-      }
-    }
-  });
+    });
 
-  $.each(json, function (index, value) {
-    if (value.constructor === Array) {
-      json[index] = value.join();
-    }
-  });
+    $.each(json, function(index, value) {
+        if (value.constructor === Array) {
+            json[index] = value.join();
+        }
+    });
 
-  return json;
+    return json;
 }
