f2428beca6f1ca242f6e0bf126ad842acd963a31
Unknown <adrian.orenstein@gmail.com>
2018-01-14 19:11:25 +1030

    WIP - added new Date().getFullYear() + "/"

--- js/csclub.raffle.js
+++ js/csclub.raffle.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
-  getWinner();
-
+	getWinner()
 })
 
 function writeWinner(text) {
-  $('#winner').text(text);
+	$("#winner").text(text)
 }
 
 function getWinner() {
-  writeWinner("I'm working on it");
-  var members = firebase.database().ref("members");
-  members.once('value').then(function(snapshot) {
-    
-    var haveWinner = false;
-    while (!haveWinner) {
-      var i = 0;
-      var rand = Math.floor(Math.random() * snapshot.numChildren());
-      snapshot.forEach(function(snapshot) {
-        if (i == rand) {
-          if (snapshot.val().square_receipt.length > 0) {
-            haveWinner = true;
-            writeWinner(snapshot.val().first_name + " " + snapshot.val().last_name);
-          } 
-        }
-        i++;
-      });
-    }
-  }).catch(function(error) {
-    writeWinner("Sorry, you don't have permission to do this."); 
-  });
+	writeWinner("I'm working on it")
+	var members = firebase.database().ref(new Date().getFullYear() + "/")
+	members
+		.once("value")
+		.then(function(snapshot) {
+			var haveWinner = false
+			while (!haveWinner) {
+				var i = 0
+				var rand = Math.floor(Math.random() * snapshot.numChildren())
+				snapshot.forEach(function(snapshot) {
+					if (i == rand) {
+						if (snapshot.val().square_receipt.length > 0) {
+							haveWinner = true
+							writeWinner(snapshot.val().first_name + " " + snapshot.val().last_name)
+						}
+					}
+					i++
+				})
+			}
+		})
+		.catch(function(error) {
+			writeWinner("Sorry, you don't have permission to do this.")
+		})
 }
